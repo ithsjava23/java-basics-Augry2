4aa58e60d17fe779170ed04e2ccf3b5411662bbe
august <august.rydnell@gmail.com>
2023-09-04 22:49:30 +0200

    changed the array to a 2d array, added method for sorting 2d array, added method for printing 2d array

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,35 +4,64 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    final static int ROWS = 24;
+    final static int COLUMNS = 3;
+    final static int HOUR_START = 0;
+    final static int HOUR_STOP = 1;
+    final static int PRICE = 2;
+
+
 
     public static void main(String[] args) {
         Scanner fetch = new Scanner(System.in);
-        int[] priceList = new int[24];
+        int[][] priceList = new int[ROWS][COLUMNS];
 
         showMenu(fetch, priceList);
 
     }
 
-    private static void sortList(int[] arr){
-
-        for (int i = 0; i < arr.length - 1 ; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-
-            }
-        }
-
+    private static void minMaxAvg(int[][] arr) {
+        Print2dList(arr);
+        bubbleSort2dArray(arr); // sorting the array from small -> big
+        System.out.println("SORTED");
+        Print2dList(arr);
 
     }
 
+    private static void Print2dList(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            // Print a newline after each row
+            System.out.println();
+        }
+    }
+
+    private static void bubbleSort2dArray(int[][] arr) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < ROWS - i - 1; j++) {
+                if (arr[j][2] > arr[j + 1][2]) {
+                    int[] temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
     // for testing purpose
-    private static int[] handleInputRandomNr() {
+    private static int[][] handleInputRandomNr() {
 
         Random rand = new Random();
-        int[] tempList = new int[24];
+        int[][] tempList = new int[ROWS][COLUMNS];
 
         for (int i = 0; i < tempList.length; i++) {
-            int randomNr = rand.nextInt(25)+1; // generate random nr between 1 - 25
-            tempList[i] = randomNr;
+            int randomNr = rand.nextInt(25) + 1; // generate random nr between 1 - 25
+            tempList[i][HOUR_START] = i;
+            tempList[i][HOUR_STOP] = i + 1;
+            tempList[i][PRICE] = randomNr;
         }
         return tempList;
     }
@@ -66,7 +95,7 @@ public class App {
 
     }
 
-    private static void showMenu(Scanner fetch, int[] priceList) {
+    private static void showMenu(Scanner fetch, int[][] priceList) {
         boolean runMenu = true;
         do {
             System.out.print("Elpriser\n" +
@@ -88,7 +117,7 @@ public class App {
                 }
 
                 case "2": {
-
+                    minMaxAvg(priceList);
                     break;
                 }
                 case "3": {
